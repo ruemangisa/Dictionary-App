01bd3110483f3485d7efa23d93d2dae222a4a3ed
Ruth Mangisa <77462324+ruemangisa@users.noreply.github.com>
2024-03-11 09:06:45 +0000

    Corrected an error

--- Script/main.js
+++ Script/main.js
@@ -1,6 +1,5 @@
-
-function getWord(word){ {
-let Apikey = "d72ad72bc9msh0d84f51770ae1c6p1c40acjsnf0691c0379d3";
-let Apiurl = "https://wordsapiv1.p.mashape.com/words/";
-  console.log(Apiurl);  
+function getWord(word) {
+  let Apikey = "d72ad72bc9msh0d84f51770ae1c6p1c40acjsnf0691c0379d3";
+  let Apiurl = "https://wordsapiv1.p.mashape.com/words/";
+  console.log(Apiurl);
 }
