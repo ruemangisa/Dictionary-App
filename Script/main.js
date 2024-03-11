102e40e14897841deeea6399d295200fccc84279
Ruth Mangisa <77462324+ruemangisa@users.noreply.github.com>
2024-03-11 09:08:36 +0000

    Added data

--- Script/main.js
+++ Script/main.js
@@ -1,5 +1,5 @@
 function getWord(word) {
   let Apikey = "d72ad72bc9msh0d84f51770ae1c6p1c40acjsnf0691c0379d3";
-  let Apiurl = "https://wordsapiv1.p.mashape.com/words/";
+  let Apiurl = "https://wordsapiv1.p.mashape.com/words/honey";
   console.log(Apiurl);
 }
